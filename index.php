4eb7fd72001f87326163f750bd4db4f6719de237
lannel <jean.lannel@hotmail.com>
2018-04-04 16:17:17 +0200

    Functions

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 include "./includes/header.php";
 
+
+
+if (isset($_GET["page"]) && $_GET["page"] != ""){
+    $page = $_GET["page"];
+}
+
+else {
+    $page = "home";
+}
+
+$page = "./includes/" . $page . ".php";
+
+include $page;
+
+
 include "./includes/footer.php";
