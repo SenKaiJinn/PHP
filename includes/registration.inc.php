c0abc52d0c1e7317441fea12955208d15351968e
lannel <jean.lannel@hotmail.com>
2018-04-05 09:28:34 +0200

    formulaire

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,1 +1,24 @@
-<h1>registration</h1>
+<form method ="post" action="#">
+    <div>
+<label for ="nom">Nom&nbsp;:</label>
+<input type ="text" name="nom"/>
+    </div>
+    <div>
+<label for ="prenom">Prénom&nbsp;:</label>
+<input type ="text" name="prenom"/>
+    </div>
+    <div>
+<label for ="mail">Mail&nbsp;:</label>
+<input type ="text" name="mail"/>
+    </div>
+    <div>
+<label for ="mdp">Mot de passe&nbsp;:</label>
+<input type ="password" name="mdp"/>
+    </div>
+    <div>
+        <input type="reset" value ="Effacer" />
+        <input type="submit" value="Envoyer"/>
+        </div>
+<input type="hidden" name="frmRegistration" />
+
+</form>
