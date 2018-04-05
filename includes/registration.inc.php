3ab437339db4ce35d55973b783f1a2c8df7f2ef4
lannel <jean.lannel@hotmail.com>
2018-04-05 13:38:35 +0200

    formulaire

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,24 +1,73 @@
-<form method ="post" action="#">
-    <div>
-<label for ="nom">Nom&nbsp;:</label>
-<input type ="text" name="nom"/>
-    </div>
-    <div>
-<label for ="prenom">Prénom&nbsp;:</label>
-<input type ="text" name="prenom"/>
-    </div>
-    <div>
-<label for ="mail">Mail&nbsp;:</label>
-<input type ="text" name="mail"/>
-    </div>
-    <div>
-<label for ="mdp">Mot de passe&nbsp;:</label>
-<input type ="password" name="mdp"/>
-    </div>
-    <div>
-        <input type="reset" value ="Effacer" />
-        <input type="submit" value="Envoyer"/>
-        </div>
-<input type="hidden" name="frmRegistration" />
+<h1>Inscription</h1>
+<?php
 
-</form>
+
+
+if (isset($_POST["frmRegistration"])) {
+   // Syntaxe classique
+/*    if (isset($_POST["nom"])) {
+    $nom = $_POST["nom"];
+}
+
+else {
+    $nom = "" ;
+}
+*/
+
+
+
+//Opérateur ternaire
+//$nom = isset($_POST["nom"]) ? $_POST["nom"] : "";
+
+// Opérateur Null coalescent PHP 7
+$nom = $_POST["nom"] ?? "";
+$prenom = $_POST["prenom"] ?? "";
+$mail = $_POST["mail"] ?? "";
+$mdp = $_POST["mdp"] ?? "";
+ 
+
+$erreurs = array();
+
+
+if ($nom == "") array_push($erreurs, "Veuillez saisir votre nom");
+if ($prenom == "") array_push($erreurs, "Veuillez saisir votre prenom");
+if ($mail == "") array_push($erreurs, "Veuillez saisir votre mail");
+if ($mdp == "") array_push($erreurs, "Veuillez saisir votre mot de passe");
+
+
+    if (count($erreurs) > 0){
+        $message = "<ul>";
+        for ($i = 0; $i < count($erreurs); $i++) {
+            $message .= "<li>"; 
+            $message .= $erreurs[$i];
+            $message .= "</li>";
+            
+        }
+
+/* deuxiéme methode 
+
+foreach($erreurs as $ligneMessage){
+    $message .= "<li>" ;
+    $message .= $ligneMessage;
+    $message .= "</li>";
+}
+  */    
+
+        $message .= "</ul>";
+        echo $message;
+    }
+
+    else {
+        echo "Pas d'erreurs";
+    }
+
+
+
+}
+
+else {
+    echo "Je ne viens pas du formulaire";
+    include "frmRegistration.php";
+}
+
+?>
